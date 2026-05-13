421c2de2acaf642158c29ae48a2c1897ef1aa78b
crumblexapp <crumbleappco@gmail.com>
2026-05-13 11:00:26 +0200

    apple sign in fix

--- lib/native-apple-sign-in.ts
+++ lib/native-apple-sign-in.ts
@@ -1,18 +1,1 @@
-import { registerPlugin } from "@capacitor/core";
-
-type AppleSignInResponse = {
-  identityToken?: string;
-  email?: string;
-  givenName?: string;
-  familyName?: string;
-};
-
-type AppleSignInPlugin = {
-  authorize(options: {
-    clientId: string;
-    redirectURI: string;
-    scopes?: string;
-  }): Promise<{ response: AppleSignInResponse }>;
-};
-
-export const SignInWithApple = registerPlugin<AppleSignInPlugin>("SignInWithApple");
+export { SignInWithApple } from "@capacitor-community/apple-sign-in";
